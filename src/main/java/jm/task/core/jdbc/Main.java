d0150e87fa30391c4e28645aaf23713667f86520
Keni3002 <4.fun.denis@gmail.com>
2023-12-13 23:35:31 +0700

    test

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -16,6 +16,7 @@ public class Main {
         System.out.println(userDaoJDBC.getAllUsers());
         userDaoJDBC.cleanUsersTable();
         userDaoJDBC.dropUsersTable();
+        System.out.println("Finish");
 
     }
 }
