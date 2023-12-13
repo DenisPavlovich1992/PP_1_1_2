43a7b0299bc1e7779a1ccf1c2e2545d4d07e875e
Keni3002 <4.fun.denis@gmail.com>
2023-12-13 23:34:10 +0700

    test

---
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -0,0 +1,21 @@
+package jm.task.core.jdbc;
+
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+public class Main {
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
+        UserServiceImpl userDaoJDBC = new UserServiceImpl();
+        userDaoJDBC.createUsersTable();
+        userDaoJDBC.saveUser("Sasha", "Ivanov", (byte) 25);
+        userDaoJDBC.saveUser("Ivan", "Bogomolov", (byte) 51);
+        userDaoJDBC.saveUser("Zheka", "Kruchinin", (byte) 23);
+        userDaoJDBC.saveUser("Gosha", "Klatov", (byte) 19);
+        System.out.println(userDaoJDBC.getAllUsers());
+        userDaoJDBC.cleanUsersTable();
+        userDaoJDBC.dropUsersTable();
+
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,24 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    private static final String DB_URL = "jdbc:mysql://localhost/kata";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "08523146";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("connect ok");
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            System.out.println("connect error");
+        }
+        return connection;
+    }
+}
