0be0ff73246dac702279516ef45ea18c001a42f5
Keni3002 <4.fun.denis@gmail.com>
2023-12-15 18:11:53 +0700

    закомментирован код для hibernate

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -5,25 +5,24 @@ import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
+/*        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
         userDaoHibernate.createUsersTable();
         userDaoHibernate.saveUser("Sasha", "Ivanov", (byte) 25);
         userDaoHibernate.saveUser("Ivan", "Bogomolov", (byte) 51);
         userDaoHibernate.saveUser("Zheka", "Kruchinin", (byte) 23);
         userDaoHibernate.saveUser("Gosha", "Klatov", (byte) 19);
-        //userDaoHibernate.removeUserById(1);
-        //userDaoHibernate.cleanUsersTable();
-        //userDaoHibernate.dropUsersTable();
+        userDaoHibernate.removeUserById(1);
+        userDaoHibernate.cleanUsersTable();
+        userDaoHibernate.dropUsersTable();*/
 
-        //System.out.println(userDaoHibernate.getAllUsers());
-       /* UserServiceImpl userDaoJDBC = new UserServiceImpl();
+        UserServiceImpl userDaoJDBC = new UserServiceImpl();
         userDaoJDBC.dropUsersTable();
         userDaoJDBC.createUsersTable();
         userDaoJDBC.saveUser("Sasha", "Ivanov", (byte) 25);
         userDaoJDBC.saveUser("Ivan", "Bogomolov", (byte) 51);
         userDaoJDBC.saveUser("Zheka", "Kruchinin", (byte) 23);
         userDaoJDBC.saveUser("Gosha", "Klatov", (byte) 19);
-        System.out.println(userDaoJDBC.getAllUsers());*/
+        System.out.println(userDaoJDBC.getAllUsers());
 
     }
 }
