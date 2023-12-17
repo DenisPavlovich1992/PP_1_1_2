af9ae199906a237793ab77188fd5331048b2de45
Keni3002 <4.fun.denis@gmail.com>
2023-12-17 12:01:48 +0700

    UserDaoHibernateImpl реализует методы UserServiceImpl исполняет методы fix main

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,17 +1,18 @@
 package jm.task.core.jdbc;
 
 import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
-        userDaoHibernate.createUsersTable();
-        userDaoHibernate.saveUser("Sasha", "Ivanov", (byte) 25);
-        userDaoHibernate.saveUser("Ivan", "Bogomolov", (byte) 51);
-        userDaoHibernate.saveUser("Zheka", "Kruchinin", (byte) 23);
-        userDaoHibernate.saveUser("Gosha", "Klatov", (byte) 19);
-        System.out.println(userDaoHibernate.getAllUsers());
-        userDaoHibernate.cleanUsersTable();
-        userDaoHibernate.dropUsersTable();
+        UserServiceImpl userServiceImpl = new UserServiceImpl();
+        userServiceImpl.createUsersTable();
+        userServiceImpl.saveUser("Sasha", "Ivanov", (byte) 25);
+        userServiceImpl.saveUser("Ivan", "Bogomolov", (byte) 51);
+        userServiceImpl.saveUser("Zheka", "Kruchinin", (byte) 23);
+        userServiceImpl.saveUser("Gosha", "Klatov", (byte) 19);
+        System.out.println(userServiceImpl.getAllUsers());
+        userServiceImpl.cleanUsersTable();
+        userServiceImpl.dropUsersTable();
     }
 }
